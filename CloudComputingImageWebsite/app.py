3958ef9b4706af0b4a1aaeaaa74057b142ee391a
AlexanderJohnRobertson <146436623+AlexanderJohnRobertson@users.noreply.github.com>
2024-07-04 19:13:47 +0100

    added cloud readiness assessment report

--- CloudComputingImageWebsite/app.py
+++ CloudComputingImageWebsite/app.py
@@ -11,6 +11,10 @@ def hello_world():  # put application's code here
 def aws_architectural_diagram_html():
     return render_template('AWS Architectural Diagram.drawio.html')
 
+@app.route('/cloudreadinessassessment')
+def cloud_readiness_assessment():
+    return render_template('cloudreadinessassessment.html')
+
 
 if __name__ == '__main__':
     app.run()
